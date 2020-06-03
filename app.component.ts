219c79ae29e36efec0424b9dffe87ff01706c74a
David Rędzia <64521895+Espouse2@users.noreply.github.com>
2020-06-03 21:15:36 +0200

    Continuation with my tasks
    Created passenger dashboard as per instructions in the Angular Ultimate course.

--- app.component.ts
+++ app.component.ts
@@ -1,49 +1,13 @@
 import { Component } from '@angular/core';
 
-interface Passenger {
-  id: number;
-  fullname: string;
-  checkedIn: boolean;
-}
-
 @Component({
   selector: 'app-root',
   styleUrls: ['./app.component.scss'],
   template: `
     <div class="app">
-      <h3>Airline Passengers</h3>
-      <ul>
-        <li *ngFor="let passenger of passengers; let i = index;">
-          <span 
-          class="status"
-          [ngClass]="{ 'checked-in': passenger.checkedIn }"></span>
-          {{ passenger.id }}: {{ passenger.fullname }}
-        </li>
-      </ul>
+      <passenger-dashboard></passenger-dashboard>
     </div>
   `
 })
 
-export class AppComponent {
-  passengers: Passenger[] = [{
-    id: 1,
-    fullname: 'Stephen',
-    checkedIn: true
-  }, {
-    id: 2,
-    fullname: 'Rose',
-    checkedIn: false
-  }, {
-    id: 3,
-    fullname: 'James',
-    checkedIn: true
-  }, {
-    id: 4,
-    fullname: 'Tina',
-    checkedIn: false
-  }, {
-    id: 5,
-    fullname: 'Louise',
-    checkedIn: true
-  }];
-}
+export class AppComponent {}
